7b55e4bfeeac4a98670f6511fe8e586cfc70e7f2
Snakatakka <etwork563@gmail.com>
2026-09-13 19:22:01 -0400

    general code architechture improvements

--- Scenes/Nodes/Sheep/Sheep.gd
+++ Scenes/Nodes/Sheep/Sheep.gd
@@ -1,17 +1,5 @@
 extends Node2D
 
-# Determines how happy the sheep is.
-# I ripped these straight from neopets with no shame.
-enum Morale {
-	DEPRESSED = -2,
-	MISERABLE,
-	SAD,
-	CONTENT,
-	HAPPY,
-	CHEERFUL,
-	OVERJOYED,
-}
-
 # The sheep's sex.
 enum Sex {
 	MALE,
@@ -39,21 +27,37 @@ enum SleepTime {
 	NIGHT
 }
 
+# Determines how happy the sheep is.
+# I ripped these straight from Neopets with no shame.
+enum Morale {
+	DEPRESSED = -2,
+	MISERABLE,
+	SAD,
+	CONTENT,
+	HAPPY,
+	CHEERFUL,
+	OVERJOYED,
+}
+
+
 @onready var movement_cooldown : Timer = $MovementCooldown
+@onready var sight_radius : Area2D = $SightRadius
 
 # These are things that WON'T change about the sheep during its lifespan. (Unless changed by a mutation.)
 var sheep_name : String = "Dolly"
 var sex : int = Sex.INTERSEX
 var sleep_time : int = SleepTime.NIGHT
-var can_breed : bool = true
+var able_to_breed : bool = true
+var morale_can_change : bool = true
+var life_stage_can_change : bool = true
 
 # These are things that MIGHT change about the sheep during its lifespan.
 var current_state : int = State.WANDER
 var current_life_stage : int = LifeStage.CHILD
 var current_morale : int = Morale.CONTENT
 var current_age : int = 0
-var breeding_age : bool = false
-
+var can_breed : bool = false # can_breed and able_to_breed are DIFFERENT. able_to_breed is permanent, can_breed isn't.
+var search_target_group : String = ""
 
 # Handles movement.
 const MAX_MOVE_DISTANCE : float = 100.0
@@ -71,20 +75,44 @@ signal state_changed(state : int)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Events.day_changed.connect(_age_up)
-	Events.day_changed.connect(_hunger_drain)
+	Events.time_changed.connect(_morale_check)
+	Events.time_changed.connect(_sleep_check)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	match current_state:
+	match current_state: # Being asleep and being dead are functionally the same, so we don't need to worry about that for now.
 		State.WANDER:
-			_random_walk()
+			if (can_move) && (!moving):
+				_random_walk()
 			return
+		State.SEARCH:
+			if (can_move) && (!moving):
+				var result : Array = _locate_target_group(search_target_group)
+				if result[0] == true: 
+					_move(result[1].position, SPEED) # If a node that is in the group is found, move to it.
+				else:
+					_random_walk() # Otherwise, move in a random direction and look again.
 
 func _random_walk() -> void:
-	if (can_move) && (!moving):
-		var delta_x : float = Math._random_float((position.x - MAX_MOVE_DISTANCE), (position.x + MAX_MOVE_DISTANCE))
-		var delta_y : float = Math._random_float((position.y - MAX_MOVE_DISTANCE), (position.y + MAX_MOVE_DISTANCE))
-		_move(Vector2(delta_x, delta_y), SPEED)
+	var delta_x : float = Math._random_float((position.x - MAX_MOVE_DISTANCE), (position.x + MAX_MOVE_DISTANCE))
+	var delta_y : float = Math._random_float((position.y - MAX_MOVE_DISTANCE), (position.y + MAX_MOVE_DISTANCE))
+	_move(Vector2(delta_x, delta_y), SPEED)
+
+func _locate_target_group(group : String) -> Array:
+	var return_array : Array = []
+	var target_found : bool = false
+	var target : Node = null
+	
+	for body in sight_radius:
+		if body.is_in_group(search_target_group):
+			_move(body.global_position, SPEED)
+			target_found = true
+			target = body
+			break
+	
+	return_array[0] = target_found
+	return_array[1] = target
+	return return_array
 
 func _move(target : Vector2, speed : float) -> void:
 	var tween : Tween = create_tween()
@@ -105,26 +133,32 @@ func _change_state(state : int) -> void:
 	state_changed.emit(current_state)
 
 func _change_life_stage(life_stage : int) -> void:
-	current_life_stage = life_stage
+	if life_stage_can_change:
+		current_life_stage = life_stage
+		if current_life_stage == LifeStage.DEAD: # Prevents sheep who are dead from rising from the grave.
+			life_stage_can_change = false
 
 func _age_up() -> void:
-	current_age += 1
-	_change_age(current_age)
+	if current_life_stage != LifeStage.DEAD: # Prevents sheep who are dead from getting any older.
+		current_age += 1
+		_change_age(current_age)
 
 func _change_age(age : int) -> void:
 	match age:
 		5:
 			_change_life_stage(LifeStage.ADULT)
-			breeding_age = true # Prevents adults from breeding with children.
 		40:
 			_change_life_stage(LifeStage.DEAD)
 		_:
 			if age > 30:
-				_death_roll(age, morale)
+				_death_roll(age, current_morale)
 
-func _morale_change(morale_level : int) -> void:
-	if morale_level < Morale.CONTENT:
-		can_breed = false
+func _change_morale(morale : int) -> void:
+	if morale_can_change:
+		current_morale = morale
+	
+	if morale < Morale.CONTENT:
+		can_breed = false # Makes it so that sheep that are sad can't breed; better incentivizes the player to keep their sheep happy.
 
 # Checks whether or not a sheep dies when it ages up; the older a sheep is, the more likely it is to die.
 func _death_roll(age : int, morale_level : int) -> void:
@@ -133,8 +167,15 @@ func _death_roll(age : int, morale_level : int) -> void:
 	if roll + morale_level < age: # Makes it so that the happier your sheep is, the less likely it is to die.
 		_change_life_stage(LifeStage.DEAD)
 
-func _hunger_drain() -> void:
+func _morale_check() -> void:
 	pass
+
+func _sleep_check(time : int) -> void:
+		if time == sleep_time:
+			_change_state(State.ASLEEP)
 
 func _on_movement_cooldown_timeout() -> void:
 	can_move = true
+
+func _on_breeding_cooldown_timeout() -> void:
+	can_breed = true
